db72190e10a0490c75a667bf038d3cfb17a9f30d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 14:38:17 +0100

    code

---
+++ src/main/java/com/thym/heroku/controllers/NamesController.java
@@ -0,0 +1,28 @@
+package com.thym.heroku.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class NamesController {
+static ArrayList<String> listNom=new ArrayList<String>();
+@GetMapping("")
+public String getHomePage()
+{
+return "index";	
+}
+@PostMapping("affichage")
+public String lister(Model md, @RequestParam String nom)
+{
+listNom.add(nom);
+md.addAttribute("liste",listNom);
+return "liste";
+
+}
+
+}

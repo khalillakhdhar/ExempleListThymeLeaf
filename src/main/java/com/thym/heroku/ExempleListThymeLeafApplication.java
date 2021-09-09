e519eae21a40ec8dcdae4ef05b40175d0517ac36
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 14:26:13 +0100

    Initial commit

---
+++ src/main/java/com/thym/heroku/ExempleListThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.thym.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExempleListThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExempleListThymeLeafApplication.class, args);
+	}
+
+}

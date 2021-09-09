acbd144286e033b9c03eb861b900da2868cb010b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 15:28:18 +0100

    envoie

--- src/main/java/com/thym/heroku/controllers/NamesController.java
+++ src/main/java/com/thym/heroku/controllers/NamesController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.thym.heroku.classes.Participant;
+
 @Controller
 public class NamesController {
 static ArrayList<String> listNom=new ArrayList<String>();
+static ArrayList<String> listparticipants=new  ArrayList<String>();
 @GetMapping("")
 public String getHomePage()
 {
@@ -28,6 +31,14 @@ return "liste";
 public String getCourse()
 {
 return "course";	
+}
+@PostMapping("course")
+public String makelist(Model m, @RequestParam String nom,String prenom,int age)
+{
+	Participant p=new Participant(nom, prenom, age);
+	listparticipants.add(p.toString());
+	m.addAttribute("liste",listparticipants);
+	return "course";
 }
 
 }
